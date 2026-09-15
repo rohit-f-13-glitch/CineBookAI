8bdb63284ebd4c036262d19014e1842989eaa9dd
Rohit <favade.r6000@gmail.com>
2026-09-15 11:15:28 +0530

    Connect Render to Supabase PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -146,11 +146,14 @@ WSGI_APPLICATION = "config.wsgi.application"
 # =========================================================
 # DATABASE
 # =========================================================
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres.xqwqdlrdohfwyppebrjj",
+        "PASSWORD": os.getenv("SUPABASE_DB_PASSWORD"),
+        "HOST": "aws-0-ap-south-1.pooler.supabase.com",
+        "PORT": "5432",
     }
 }
 
